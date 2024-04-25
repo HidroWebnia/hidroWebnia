67a81ded57fb90c7d7fab43211341cecf42652ac
Joaol324j <joaol324j@gmail.com>
2024-04-25 08:33:07 -0300

    Atualização

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3080/api'
+    baseURL: 'https://devicesserver.onrender.com/api/'
 });
 
 export default api;
 
 export const deleteRegister = (id) => {
-    return axios.delete(`http://localhost:3080/api/devices/${id}`)
+    return axios.delete(`https://devicesserver.onrender.com/api/devices/${id}`)
         .then(response => {
             console.log(response)
             window.location.reload()
@@ -15,8 +15,8 @@ export const deleteRegister = (id) => {
         .catch(err => console.log(err))
 }
 
-export const editRegister = (id, { name, description, image }) => {
-    return axios.patch(`http://localhost:3080/api/devices/${id}`, { name, description, image }) 
+export const editRegister = (id, { name, image }) => {
+    return axios.patch(`https://devicesserver.onrender.com/api/devices/${id}`, { name, image }) 
         .then(response => {
             console.log('Editado', response)
             window.location.reload()
@@ -24,8 +24,8 @@ export const editRegister = (id, { name, description, image }) => {
         .catch(err => console.log(err))
 }
 
-export const addRegister = ({name, description, email, image}) => {
-    return axios.post(`http://localhost:3080/api/devices`, { name, description, email, image }) 
+export const addRegister = ({name, email, image}) => {
+    return axios.post(`https://devicesserver.onrender.com/api/devices`, { name, email, image }) 
         .then(response => {
             console.log('Adicionado', response)
             window.location.reload()
