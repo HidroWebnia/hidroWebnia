00b3f4d7a3a14a8bffff27016e4b6e5b6b343906
Joaol324j <joaol324j@gmail.com>
2024-04-24 23:25:42 -0300

    update no app

--- app.js
+++ app.js
@@ -10,15 +10,6 @@ app.use('/api', api)
 
 const port = process.env.PORT 
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'))
-
-    const path = require('path')
-    app.get('*', (req, res => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))}
-    ))
-}
-
 app.listen(port, () => {
     console.log('App is Running')
 })
