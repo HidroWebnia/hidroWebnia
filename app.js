6dfe4dae9a185f985a2d31bf739637d392efc0b5
Joaol324j <joaol324j@gmail.com>
2024-04-30 09:18:54 -0300

    Att 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const cors = require('cors')
 const api = require('./rotas/index')
 
 app.use(cors())
